d987809d5c2a3d2a0133ffe8a1cfe20544c7d366
nycpivot <mijames@vmware.com>
2020-10-18 19:26:34 -0400

    Added additional scripts.

--- usecases/quickstarts/use-case-01/script1.sh
+++ usecases/quickstarts/use-case-01/script1.sh
@@ -2,6 +2,8 @@ tkg delete cluster use-case-01
 tkg create cluster use-case-01 --kubernetes-version=1.17.9 --plan=prod
 tkg get credentials use-case-01
 
+kubectl config use-context use-case-01-admin@use-case-01
+
 kubectl apply -f https://raw.githubusercontent.com/nycpivot/elastic-cloud-kubernetes/main/common/storage.yaml
 kubectl apply -f https://raw.githubusercontent.com/nycpivot/elastic-cloud-kubernetes/main/common/pv.yaml
 kubectl apply -f https://raw.githubusercontent.com/nycpivot/elastic-cloud-kubernetes/main/artifacts/all-in-one.yaml
